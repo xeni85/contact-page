f237869068d654ac6577e3e52aa8cf79650e64c8
Xeni <shkelzen.dunisha@umsh.edu.al>
2022-04-23 22:49:27 -0400

    stone tech solutions contact form

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var password = document.querySelector("#password")
+  , confirm_password = document.querySelector("#confirmPassword");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
